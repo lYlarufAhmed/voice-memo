d35122f364359200e45593720f4082e557651dcb
maruf-ahmed <ahmedmaruf474@gmail.com>
2021-09-25 12:20:22 +0600

    removed the visualizer

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,15 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
+import {makeStyles} from "@material-ui/core/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import {
-  Box,
-  List,
-  ListItemText,
-  Paper,
-  Typography,
-  useScrollTrigger,
+    Box,
+    List,
+    ListItemText,
+    Paper,
+    Typography,
+    useScrollTrigger,
 } from "@material-ui/core";
-import { useReactMediaRecorder } from "react-media-recorder";
+import {useReactMediaRecorder} from "react-media-recorder";
 import Track from "./Track";
 import AppBar from "@material-ui/core/AppBar";
 import clsx from "clsx";
@@ -19,267 +19,257 @@ import Snackbar from "@material-ui/core/Snackbar";
 import MuiAlert from "@material-ui/lab/Alert";
 
 const formattedDateTime = () => {
-  let currentDateObj = new Date();
-  return `${currentDateObj.getMonth()}-${currentDateObj.getDate()}-${currentDateObj.getFullYear()}_${currentDateObj
-    .getHours()
-    .toString()
-    .padStart(2, "0")}:${currentDateObj
-    .getMinutes()
-    .toString()
-    .padStart(2, "0")}`;
+    let currentDateObj = new Date();
+    return `${currentDateObj.getMonth()}-${currentDateObj.getDate()}-${currentDateObj.getFullYear()}_${currentDateObj
+        .getHours()
+        .toString()
+        .padStart(2, "0")}:${currentDateObj
+        .getMinutes()
+        .toString()
+        .padStart(2, "0")}`;
 };
 
 function ElevationScroll(props) {
-  const { children, window } = props;
+    const {children, window} = props;
 
-  const trigger = useScrollTrigger({
-    disableHysteresis: true,
-    threshold: 0,
-    target: window ? window() : undefined,
-  });
+    const trigger = useScrollTrigger({
+        disableHysteresis: true,
+        threshold: 0,
+        target: window ? window() : undefined,
+    });
 
-  return React.cloneElement(children, {
-    elevation: trigger ? 4 : 0,
-  });
+    return React.cloneElement(children, {
+        elevation: trigger ? 4 : 0,
+    });
 }
 
 const openDrawer = (status, paused) => status === "recording" || paused;
 const drawerHeight = 300;
 const useStyles = makeStyles((theme) => ({
-  root: {
-    display: "flex",
-    backgroundColor: "yellow",
-    height: "100vh",
-  },
-  hide: {
-    display: "none",
-  },
-  // Paper
-  text: {
-    padding: theme.spacing(2, 2, 0),
-  },
-  paper: {
-    // paddingBottom: 50,
-    flexBasis: "50%",
-    flexGrow: 1,
-    overflowY: "scroll",
-    overflowX: "hidden",
-  },
-  list: {
-    position: "relative",
-  },
-  listPadding: {
-    paddingBottom: 70,
-  },
-  flexGrow: {
-    flexGrow: 1,
-    display: "grid",
-    placeItems: "center",
-    flexBasis: "50%",
-  },
-  appBar: {
-    transition: theme.transitions.create(["margin", "height"], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-    bottom: 0,
-    top: "auto",
-    alignItems: "center",
-    backgroundColor: "white",
-  },
-  appBarShift: {
-    height: drawerHeight,
-    transition: theme.transitions.create(["margin", "height"], {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-  },
-  overlay: {
-    height: "100%",
-    backgroundColor: "black",
-    padding: "4rem",
-    position: "absolute",
-    width: "100%",
-    top: 0,
-    display: "none",
-    opacity: ".4",
-  },
-  overlayShow: {
-    display: "block",
-  },
+    root: {
+        display: "flex",
+        backgroundColor: "yellow",
+        height: "100vh",
+    },
+    hide: {
+        display: "none",
+    },
+    // Paper
+    text: {
+        padding: theme.spacing(2, 2, 0),
+    },
+    paper: {
+        // paddingBottom: 50,
+        flexBasis: "50%",
+        flexGrow: 1,
+        overflowY: "scroll",
+        overflowX: "hidden",
+    },
+    list: {
+        position: "relative",
+    },
+    listPadding: {
+        paddingBottom: 70,
+    },
+    flexGrow: {
+        flexGrow: 1,
+        display: "grid",
+        placeItems: "center",
+        flexBasis: "50%",
+    },
+    appBar: {
+        transition: theme.transitions.create(["margin", "height"], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+        bottom: 0,
+        top: "auto",
+        alignItems: "center",
+        backgroundColor: "white",
+    },
+    appBarShift: {
+        height: drawerHeight,
+        transition: theme.transitions.create(["margin", "height"], {
+            easing: theme.transitions.easing.easeOut,
+            duration: theme.transitions.duration.leavingScreen,
+        }),
+    },
+    overlay: {
+        height: "100%",
+        backgroundColor: "black",
+        padding: "4rem",
+        position: "absolute",
+        width: "100%",
+        top: 0,
+        display: "none",
+        opacity: ".4",
+    },
+    overlayShow: {
+        display: "block",
+    },
 }));
 let milliSecElapsed = 0;
+
 function Alert(props) {
-  return <MuiAlert elevation={6} variant="filled" {...props} />;
+    return <MuiAlert elevation={6} variant="filled" {...props} />;
 }
 
 export default function App() {
-  const matches = useMediaQuery("(min-width:769px)");
-  const classes = useStyles();
-  const [open, setOpen] = React.useState(false);
-  // let [tracks, setTracks] = React.useState({
-  //   // 'test_track': {url: 'https://www.mfiles.co.uk/mp3-downloads/gs-cd-track2.mp3'},
-  // });
-  let [tracks, setTracks] = React.useState([
-    "https://www.mfiles.co.uk/mp3-downloads/gs-cd-track2.mp3",
-  ]);
-  let [paused, setPaused] = React.useState(false);
-  let [durationMilliSec, setDurationMilliSec] = React.useState(milliSecElapsed);
+    const matches = useMediaQuery("(min-width:769px)");
+    const classes = useStyles();
+    const [open, setOpen] = React.useState(false);
+    let [tracks, setTracks] = React.useState(new Map());
 
-  //   const recordingName = `New_Idea_${
-  //     Object.entries(tracks).length + 1
-  //   }_${formattedDateTime()}`;
-  const [recordingName, setRecordingName] = React.useState();
-  const [snackOpen, setSnackOpen] = React.useState(false);
-  const {
-    status,
-    startRecording,
-    pauseRecording,
-    stopRecording,
-    resumeRecording,
-    mediaBlobUrl,
-  } = useReactMediaRecorder({
-    audio: true,
-  });
-  React.useEffect(() => {
-    // if (mediaBlobUrl && recordingName) {
-    if (mediaBlobUrl) {
-      setTracks((prev) => {
-        if (!prev.includes(mediaBlobUrl)) {
-          // prev[mediaBlobUrl] = {
-          //   title: recordingName,
-          // };
-          prev.push(mediaBlobUrl);
-        }
-        return prev.slice();
-      });
-    }
-  }, [mediaBlobUrl, recordingName]);
+    let [paused, setPaused] = React.useState(false);
+    let [durationMilliSec, setDurationMilliSec] = React.useState(milliSecElapsed);
 
-  const deleteTrack = (title) => {
-    setTracks((prevState) => {
-      delete prevState[title];
-      return JSON.parse(JSON.stringify(prevState));
+    const [recordingName, setRecordingName] = React.useState('');
+    const [snackOpen, setSnackOpen] = React.useState(false);
+    const {
+        status,
+        startRecording,
+        pauseRecording,
+        stopRecording,
+        resumeRecording,
+        mediaBlobUrl,
+        clearBlobUrl
+    } = useReactMediaRecorder({
+        audio: true,
     });
-  };
+    React.useEffect(() => {
+        if (mediaBlobUrl) {
+            setTracks((prev) => {
+                if (!prev.has(mediaBlobUrl))
+                    prev.set(mediaBlobUrl, {title: recordingName});
+                return new Map([...prev.entries()]);
+            });
+            clearBlobUrl();
+        }
+    }, [mediaBlobUrl, clearBlobUrl, recordingName]);
 
-  const handleDrawerOpen = () => {
-    setOpen(true);
-    setRecordingName(
-      // `New_Idea_${Object.entries(tracks).length + 1}_${formattedDateTime()}`
-      `New_Idea_${tracks.length + 1}_${formattedDateTime()}`
+    const deleteTrack = (url) => {
+        setTracks((prevState) => {
+            prevState.delete(url)
+            return new Map([...prevState.entries()]);
+        });
+    };
+
+    const handleDrawerOpen = () => {
+        setOpen(true);
+        setRecordingName(
+            // `New_Idea_${Object.entries(tracks).length + 1}_${formattedDateTime()}`
+            `New_Idea_${tracks.size + 1}_${formattedDateTime()}`
+        );
+        startRecording();
+    };
+
+    const handleDrawerClose = () => {
+        setOpen(false);
+        stopRecording();
+        setPaused(false);
+        setDurationMilliSec(0);
+        setSnackOpen(true);
+    };
+    const handlePause = async () => {
+        try {
+            await pauseRecording();
+            setPaused(true);
+            milliSecElapsed = durationMilliSec;
+        } catch (e) {
+            console.log(e.message);
+        }
+    };
+    const handleResume = async () => {
+        try {
+            await resumeRecording();
+            setPaused(false);
+            setDurationMilliSec(milliSecElapsed);
+        } catch (e) {
+            console.log(e.message);
+        }
+    };
+
+    const handleSnackClose = () => setSnackOpen(false);
+
+    return (
+        <div className={classes.root}>
+            <CssBaseline/>
+            <Snackbar
+                // anchorOrigin={{ vertical, horizontal }}
+                open={snackOpen}
+                onClose={handleSnackClose}
+                autoHideDuration={3000}
+            >
+                <Alert severity="success" onClose={handleSnackClose}>
+                    Recording Saved!
+                </Alert>
+            </Snackbar>
+            <Paper square className={classes.paper}>
+                <List
+                    className={clsx(classes.list, {[classes.listPadding]: !matches})}
+                >
+                    {matches && <ListItemText>Desktop View</ListItemText>}
+                    {tracks.size > 0 ? [...tracks.entries()].map(([url, {title}], index) => (
+                            <Track
+                                key={url}
+                                url={url}
+                                count={index}
+                                title={title}
+                                handleDelete={() => deleteTrack(url)}
+                            />
+                        ))
+                        : (
+                            <Typography variant={"h4"} align={"center"}>
+                                No items
+                            </Typography>
+                        )}
+                    {!matches && (
+                        <Box
+                            className={clsx(classes.overlay, {
+                                [classes.overlayShow]: open,
+                            })}
+                        />
+                    )}
+                </List>
+            </Paper>
+            {matches ? (
+                <Paper className={classes.flexGrow}>
+                    <RecorderContainer
+                        open={open}
+                        paused={paused}
+                        handleDrawerOpen={handleDrawerOpen}
+                        handleDrawerClose={handleDrawerClose}
+                        pauseRecording={handlePause}
+                        recordingName={recordingName}
+                        durationMilliSec={durationMilliSec}
+                        setDurationMilliSec={setDurationMilliSec}
+                        resumeRecording={handleResume}
+                        isDrawerOpen={openDrawer(status, paused)}
+                    />
+                </Paper>
+            ) : (
+                <ElevationScroll>
+                    <AppBar
+                        position="fixed"
+                        className={clsx(classes.appBar, {
+                            [classes.appBarShift]: open,
+                        })}
+                    >
+                        <RecorderContainer
+                            open={open}
+                            paused={paused}
+                            handleDrawerOpen={handleDrawerOpen}
+                            handleDrawerClose={handleDrawerClose}
+                            pauseRecording={handlePause}
+                            recordingName={recordingName}
+                            durationMilliSec={durationMilliSec}
+                            setDurationMilliSec={setDurationMilliSec}
+                            resumeRecording={handleResume}
+                            isDrawerOpen={openDrawer(status, paused)}
+                        />
+                    </AppBar>
+                </ElevationScroll>
+            )}
+        </div>
     );
-    startRecording();
-  };
-
-  const handleDrawerClose = () => {
-    setOpen(false);
-    stopRecording();
-    setPaused(false);
-    setDurationMilliSec(0);
-    setSnackOpen(true);
-  };
-  const handlePause = async () => {
-    try {
-      await pauseRecording();
-      setPaused(true);
-      milliSecElapsed = durationMilliSec;
-    } catch (e) {
-      console.log(e.message);
-    }
-  };
-  const handleResume = async () => {
-    try {
-      await resumeRecording();
-      setPaused(false);
-      setDurationMilliSec(milliSecElapsed);
-    } catch (e) {
-      console.log(e.message);
-    }
-  };
-
-  const handleSnackClose = () => setSnackOpen(false);
-
-  return (
-    <div className={classes.root}>
-      <CssBaseline />
-      <Snackbar
-        // anchorOrigin={{ vertical, horizontal }}
-        open={snackOpen}
-        onClose={handleSnackClose}
-        autoHideDuration={3000}
-      >
-        <Alert severity="success" onClose={handleSnackClose}>
-          Recording Saved!
-        </Alert>
-      </Snackbar>
-      <Paper square className={classes.paper}>
-        <List
-          className={clsx(classes.list, { [classes.listPadding]: !matches })}
-        >
-          {matches && <ListItemText>Desktop View</ListItemText>}
-          {tracks.length ? (
-            tracks.map((url, i) => (
-              <Track
-                url={url}
-                title={`New recording ${i}`}
-                key={url}
-                count={i}
-                handleDelete={deleteTrack}
-              />
-            ))
-          ) : (
-            <Typography variant={"h4"} align={"center"}>
-              No items
-            </Typography>
-          )}
-          {!matches && (
-            <Box
-              className={clsx(classes.overlay, {
-                [classes.overlayShow]: open,
-              })}
-            />
-          )}
-        </List>
-      </Paper>
-      {matches ? (
-        <Paper className={classes.flexGrow}>
-          <RecorderContainer
-            open={open}
-            paused={paused}
-            handleDrawerOpen={handleDrawerOpen}
-            handleDrawerClose={handleDrawerClose}
-            pauseRecording={handlePause}
-            recordingName={recordingName}
-            durationMilliSec={durationMilliSec}
-            setDurationMilliSec={setDurationMilliSec}
-            resumeRecording={handleResume}
-            isDrawerOpen={openDrawer(status, paused)}
-          />
-        </Paper>
-      ) : (
-        <ElevationScroll>
-          <AppBar
-            position="fixed"
-            className={clsx(classes.appBar, {
-              [classes.appBarShift]: open,
-            })}
-          >
-            <RecorderContainer
-              open={open}
-              paused={paused}
-              handleDrawerOpen={handleDrawerOpen}
-              handleDrawerClose={handleDrawerClose}
-              pauseRecording={handlePause}
-              recordingName={recordingName}
-              durationMilliSec={durationMilliSec}
-              setDurationMilliSec={setDurationMilliSec}
-              resumeRecording={handleResume}
-              isDrawerOpen={openDrawer(status, paused)}
-            />
-          </AppBar>
-        </ElevationScroll>
-      )}
-    </div>
-  );
 }
--- src/components/RecordingDashboard.js
+++ src/components/RecordingDashboard.js
@@ -1,6 +1,5 @@
 import React from "react";
 import {Typography, Container} from "@material-ui/core";
-import Recorder from "./Recorder";
 
 
 
